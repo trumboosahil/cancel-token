1fbb8874eb58ac606a47c0ba25d9d6c0f5643c27
Shafi <trumboo@theahoyapp.com>
2022-04-10 02:45:09 +0400

    first commit

---
+++ CancelAPI/Controllers/CancelController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancelAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CancelController : ControllerBase
+    {
+        private readonly ILogger<CancelController> _logger;
+
+        private static readonly string[] Summaries = new[]
+         {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public CancelController(ILogger<CancelController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get(CancellationToken token)
+        {
+            try
+            {
+                Console.WriteLine("CancellationToken");
+                await Task.Delay(10000,token);
+                var rng = new Random();
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+            catch (TaskCanceledException ex)
+            {
+                return null;
+            }
+            return null;
+
+        }
+    }
+}
---
+++ CancelationToken/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CancelationToken
+{
+    public partial class Form1 : Form
+    {
+        CancellationTokenSource tokenSource = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+            var progress = new Progress<int>(value =>
+            {
+                progressBar1.Value = value;
+                label1.Text = $"{value}";
+
+            });
+            try
+            {
+                await Task.Run(() => IntiateProgressBar(100, progress, token));
+                label1.Text = "Complete";
+            }
+            catch (OperationCanceledException ex)
+            {
+                label1.Text = "Cancel";
+            }
+            finally
+            {
+                tokenSource.Dispose();
+            }
+            
+        }
+       
+
+        private void IntiateProgressBar(int count, IProgress<int> progress, CancellationToken token)
+        {
+            for(int i = 0; i < count; i++)
+            {
+                Thread.Sleep(100);
+                int per = (i *100) / count;
+                progress.Report(per);
+                if(token.IsCancellationRequested)
+                {
+                    token.ThrowIfCancellationRequested();
+                }
+               // progressBar1.Value = per;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tokenSource?.Cancel();
+        }
+    }
+}
